02973756279ede6474fc541b796de5ff8cf7b027
Aditi Madan <aditi@cloudinary.com>
2021-01-12 19:36:45 +0000

    Removed the env var

--- Main.java
+++ Main.java
@@ -5,9 +5,8 @@ import com.cloudinary.utils.ObjectUtils;
 
 class Main {
   public static void main(String[] args) throws Exception {
-   /* Cloudinary cloudinary = new
-		Cloudinary("cloudinary://549185954311254:LQwYOwL9Ij9ioS6lCCwdB5K7ErY@aditimadan");
-    Api api = cloudinary.api();*/
+ 
+    Api api = cloudinary.api();
     AestheticsTransformations at=new AestheticsTransformations();
     at.printTransformation();
        
